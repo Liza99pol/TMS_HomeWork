2ef4d3c15d4470d22fad64faa09875efbd31402c
Liza99pol <102958936+Liza99pol@users.noreply.github.com>
2022-07-12 17:30:28 +0300

    HW 22 (2 пункт)

---
+++ HomeWork 22/HomeWork 22/FilmModel/Film1.swift
@@ -0,0 +1,29 @@
+//
+//  Film1.swift
+//  HomeWork 22
+//
+//  Created by macbook m1 on 12.07.22.
+//
+
+import Foundation
+
+struct Films1: Codable {
+    var title: String?
+    var episode_id: Int?
+    var opening_crawl: String?
+    var director: String?
+    var producer: String?
+    var release_date: String?
+    var characters: [String]?
+    var planets: [String]?
+    var starships: [String]?
+    var vehicles: [String]?
+    var species: [String]?
+    var created: String?
+    var edited: String?
+    var url: String?
+}
+
+
+
+
